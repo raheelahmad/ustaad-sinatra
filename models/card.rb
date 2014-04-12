68f88349f3a02b2d7e80880a61db8b988b9f01d9
Raheel Ahmad <ahmadraheel@gmail.com>
2014-04-11 19:31:15 -0700

    Refactor card.update

--- models/card.rb
+++ models/card.rb
@@ -4,13 +4,17 @@ class Card
   attr_accessor :id, :front, :back, :title
 
   def initialize(params)
-    @front = params["front"]
-    @back = params["back"]
-    @title = params["title"]
-    @id = params["id"]
+    update params
   end
 
   def to_json(*a)
     { id:@id, front:@front, back:@back, title:@title }.to_json(*a)
+  end
+
+  def update(params)
+    @front = params["front"] unless params["front"].nil?
+    @back = params["back"] unless params["back"].nil?
+    @title = params["title"] unless params["title"].nil?
+    @id = params["id"].to_i unless params["id"].nil?
   end
 end
